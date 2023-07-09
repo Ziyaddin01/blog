b46569299b32456b24b4cb17abf5a3971ccba2c6
Ziyaddin01 <ziya.huseynov200101@gmail.com>
2023-07-09 16:55:16 +0400

    Delete and Edit

--- src/main/java/com/backprogram/blog/controller/BlogController.java
+++ src/main/java/com/backprogram/blog/controller/BlogController.java
@@ -51,4 +51,26 @@ public class BlogController {
 
     }
 
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+
+    }
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+
+        return "redirect:/blog";
+    }
+
+
 }
